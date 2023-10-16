ee1e2303beb8e79855c232c367c19e9846837990
Arjun Varshney <varshneyarjun49@gmail.com>
2023-10-16 07:01:42 +0530

    something

---
+++ app/(root)/secret/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const SecretPage = () => {
+   return (
+      <div className="p-6 max-w-3xl lg:max-w-6xl xl:max-w-7xl mx-auto">
+         <div className="flex justify-between space-x-5">
+            <Button
+               className="min-h-[80vh] w-full text-3xl border-8 border-primary"
+               variant={"outline"}
+            >
+               <Link
+                  href={
+                     "https://drive.google.com/drive/folders/1g-wLd-33gdxIxVym4RSGMJLehmAldMuW?usp=drive_link"
+                  }
+                  target="_blank"
+                  className="h-full w-full grid place-items-center"
+               >
+                  Tensorflow
+               </Link>
+            </Button>
+            <Button
+               className="min-h-[80vh] w-full text-3xl border-8 border-primary"
+               variant={"outline"}
+            >
+               <Link
+                  href={
+                     "https://drive.google.com/drive/folders/1M_7Z2wXyCdKfe5k_PgF9oACazyKbPdIn?usp=drive_link"
+                  }
+                  target="_blank"
+                  className="h-full w-full grid place-items-center"
+               >
+                  Pytorch
+               </Link>
+            </Button>
+         </div>
+      </div>
+   );
+};
+
+export default SecretPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,25 +9,25 @@ import ToastProvider from "@/providers/toast-provider";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Body Works",
-  description: "Exercise website for everyone",
+   title: "Body Works",
+   description: "Exercise website for everyone",
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode;
+   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className} suppressHydrationWarning={true}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <ToastProvider />
-          <NavBar />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <body className={font.className} suppressHydrationWarning={true}>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+               <ToastProvider />
+               <NavBar />
+               {children}
+               <Footer />
+            </ThemeProvider>
+         </body>
+      </html>
+   );
 }
